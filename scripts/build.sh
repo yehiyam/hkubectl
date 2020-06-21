de0297ea358e689665ac822eab3349bdedbda550
Yehiyam Livneh <yehiyam@gmail.com>
2020-06-21 17:25:25 +0300

    downgrade pkg for windows builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PLATFORM=${1:-linux}
 echo building ${PLATFORM} platform
 cp "${DIR}/../platform/syncthing-${PLATFORM}" "${DIR}/../helpers/syncthing/syncthing"
-"${DIR}/../node_modules/.bin/pkg" -t ${PLATFORM} -o "${DIR}/../output/hkubectl-${PLATFORM}" "${DIR}/../" 
+"${DIR}/../node_modules/.bin/pkg" --options no-warnings -t ${PLATFORM} -o "${DIR}/../output/hkubectl-${PLATFORM}" "${DIR}/../" 
